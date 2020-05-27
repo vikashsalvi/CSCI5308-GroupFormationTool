77eb382b30d32561b9d9025a2ea7b17573f6e3f3
Vikash Manharlal Salvi <vk299944@dal.ca>
2020-05-27 18:31:56 -0300

    Trying to add env variables

--- src/main/java/com/sprin/gradle/sample/handson/controller/RESTController.java
+++ src/main/java/com/sprin/gradle/sample/handson/controller/RESTController.java
@@ -1,18 +1,27 @@
 package com.sprin.gradle.sample.handson.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+
 @Controller
 @RequestMapping("/")
 public class RESTController {
 
+    @Value("${spring.profiles.active:NotFound}")
+    private String activeProfile;
+
     @GetMapping(path = "/")
     public @ResponseBody
     String helloWorld() {
-        return "Hello";
+        return "Hello "+activeProfile;
     }
 
 }
