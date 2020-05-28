5cf7c3524e70bce72b36280ebb3e18b60cf3aaea
Daksh <dakshpatel2298@gmail.com>
2020-05-28 18:44:09 -0300

    Added signup mapping and index routing

--- src/main/java/com/app/group15/LoginController.java
+++ src/main/java/com/app/group15/LoginController.java
@@ -1,5 +1,26 @@
 package com.app.group15;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class LoginController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup() {
+//		Returns signup page
+		return "signup";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@ModelAttribute User user) {
+		System.out.println(user);
+//		call dao for saving the user
+		return "";
+	}
 }
