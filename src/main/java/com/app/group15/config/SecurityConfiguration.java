0e6986d593f236f5e9885f38f2f7d8d0044db871
dabhi <dabhi>
2020-05-29 16:07:45 -0300

    fixed login

--- src/main/java/com/app/group15/config/SecurityConfiguration.java
+++ src/main/java/com/app/group15/config/SecurityConfiguration.java
@@ -1,21 +1,24 @@
 package com.app.group15.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+//@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("user").password("{noop}password").roles("USER") .and()
-			.withUser("admin").password("{noop}password").roles("ADMIN");
+				.withUser("user").password("{noop}password").roles("USER")
+				.and()
+				.withUser("admin").password("{noop}password").roles("ADMIN");
 	}
 
 	@Override
@@ -25,8 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.disable()
 			.authorizeRequests()
 			.antMatchers("/", "/signup","/login").permitAll()
-			.antMatchers("/admin/**").hasAnyRole("ADMIN")
-			.antMatchers("/user/**").hasAnyRole("USER")
-			.anyRequest().authenticated();
+				.anyRequest().authenticated();
 	}
+
 }
--- src/main/java/com/app/group15/controller/LoginController.java
+++ src/main/java/com/app/group15/controller/LoginController.java
@@ -1,8 +1,32 @@
 package com.app.group15.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
+
+    @PostMapping(value = "/login")
+    public String loginPost(@RequestParam(required=false, value = "bannerId") String bannerId, @RequestParam(required=false, value = "password") String password) {
+
+        String USERNAME = "B00848532";
+        String PASSWORD = "user";
+
+        // check if banner is valid user
+
+        if(bannerId.equals(USERNAME) && password.equals(PASSWORD))
+        {
+            return "index";
+        }else {
+            return "login";
+        }
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGet() {
+        return "login";
+    }
 
 }
