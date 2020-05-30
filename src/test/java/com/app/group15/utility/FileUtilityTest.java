c9a41bd3163ffcf476227d285488de02e973bded
Vikash Manharlal Salvi <vk299944@dal.ca>
2020-05-30 08:10:17 -0300

    Fixing build

---
+++ src/test/java/com/app/group15/utility/FileUtilityTest.java
@@ -0,0 +1,16 @@
+package com.app.group15.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FileUtilityTest {
+
+    @Test
+    public void checkFileExistTest(){
+        String propertyFilePath = "src/main/resources/application.properties";
+        assertEquals(FileUtility.checkFileExist(propertyFilePath),true);
+    }
+}
