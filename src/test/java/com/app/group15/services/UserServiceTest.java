a5b92fc86d49a8bd0c84c5b88571da11ffe625fd
kandikonda <ss544867@dal.ca>
2020-06-03 17:54:12 -0300

    utility related changes

---
+++ src/test/java/com/app/group15/services/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.app.group15.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import com.app.group15.model.User;
+import com.app.group15.persistence.dao.UserDaoMock;
+
+
+class UserServiceTest {
+	
+	private  UserDaoMock userDaoMock = new UserDaoMock();
+	
+	@Test
+	public  void getAllUsersTest() {
+		ArrayList<User> userList=userDaoMock.getUserList();
+		userList.removeIf(user -> user.getBannerId().equals("admin"));
+		assertEquals(userList.size(), 1);
+		
+	}
+}
