c208bc62cf358b71032eeb1aa445beffe9f3d494
daksh <dk792765@dal.ca>
2020-06-04 15:40:18 -0300

    Added more test cases along with modifying some mock classes

---
+++ src/test/java/com/app/group15/services/AuthorizationServiceTest.java
@@ -0,0 +1,53 @@
+package com.app.group15.services;
+
+import com.app.group15.persistence.dao.UserRoleDaoMock;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorizationServiceTest {
+	private HttpServletRequestMock requestMock;
+	private UserRoleDaoMock userRoleDaoMock = new UserRoleDaoMock();
+	private Set<String> allowedRoles;
+
+	{
+		allowedRoles = new HashSet<>();
+		requestMock = new HttpServletRequestMock();
+	}
+
+	private void setAllowedRoles(String[] args) {
+		this.allowedRoles.clear();
+		this.allowedRoles.addAll(Arrays.asList(args));
+	}
+
+	@Test
+	void isAuthorized() {
+		Set<String> roles;
+		String bannerId;
+		Set<String> intersection;
+		int intersectionSize;
+		setAllowedRoles(new String[]{"STUDENT"});
+		requestMock.getSessionMock().setAttributeMock("BANNER_ID_SESSION","B00843468");
+		bannerId = (String) requestMock.getSessionMock().getAttributeMock("BANNER_ID_SESSION");
+		intersection = new HashSet<>(this.allowedRoles);
+		roles = userRoleDaoMock.getRolesByBannerIdMock(bannerId);
+		intersection.retainAll(roles);
+		intersectionSize = intersection.size();
+		assertNotEquals(intersectionSize,0);
+
+		setAllowedRoles(new String[]{"INSTRUCTOR"});
+		requestMock.getSessionMock().setAttributeMock("BANNER_ID_SESSION","B00843468");
+		bannerId = (String) requestMock.getSessionMock().getAttributeMock("BANNER_ID_SESSION");
+		intersection = new HashSet<>(this.allowedRoles);
+		roles = userRoleDaoMock.getRolesByBannerIdMock(bannerId);
+		intersection.retainAll(roles);
+		intersectionSize=intersection.size();
+		assertEquals(intersectionSize,0);
+
+
+	}
+}
