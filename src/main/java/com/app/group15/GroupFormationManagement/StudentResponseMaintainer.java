93a11eac0641063adfaa73e73965dfab1bcc8799
kandikonda <ss544867@dal.ca>
2020-07-10 22:31:27 -0300

    algorithm changes

---
+++ src/main/java/com/app/group15/GroupFormationManagement/StudentResponseMaintainer.java
@@ -0,0 +1,23 @@
+package com.app.group15.GroupFormationManagement;
+
+import java.util.ArrayList;
+
+public class StudentResponseMaintainer {
+
+	private int studentId;
+	private ArrayList<Object> responseList;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public ArrayList<Object> getResponseList() {
+		return responseList;
+	}
+	public void setResponseList(ArrayList<Object> responseList) {
+		this.responseList = responseList;
+	}
+	
+	
+}
