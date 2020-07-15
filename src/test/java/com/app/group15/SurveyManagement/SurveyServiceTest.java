130194c3c86e28879ce12dc21fbe7ff56dde5e0f
neelkanth <nl543396@dal.ca>
2020-07-14 21:26:15 -0300

    Bug fix to Survey module

--- src/test/java/com/app/group15/SurveyManagement/SurveyServiceTest.java
+++ src/test/java/com/app/group15/SurveyManagement/SurveyServiceTest.java
@@ -11,33 +11,33 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SurveyServiceTest {
-	private SurveyDaoMock surveyDaoMock=new SurveyDaoMock();
-	private ISurveyService surveyService= ServiceConfigForTest.getInstance().getSurveyService();
+    private SurveyDaoMock surveyDaoMock = new SurveyDaoMock();
+    private ISurveyService surveyService = ServiceConfigForTest.getInstance().getSurveyService();
 
     @Test
     void getSurveyQuestionByInstructorID() throws SQLException, AwsSecretsManagerException {
-    	List<Question> questionList=surveyService.getSurveyQuestionByInstructorID(1);
-    	int i=0;
-    	for(i=0;i<questionList.size();i++){
-			Question question = questionList.get(i);
-			assertEquals(question.getQuestionId(), i);
-		}
+        List<Question> questionList = surveyService.getSurveyQuestionByInstructorID(1);
+        int i = 0;
+        for (i = 0; i < questionList.size(); i++) {
+            Question question = questionList.get(i);
+            assertEquals(question.getQuestionId(), i);
+        }
     }
 
     @Test
     void getSurveyByCourseId() throws SQLException, AwsSecretsManagerException {
-    	Survey survey=surveyService.getSurveyByCourseId(1);
-    	assertEquals(1,survey.getId());
+        Survey survey = surveyService.getSurveyByCourseId(1);
+        assertEquals(1, survey.getId());
     }
 
     @Test
     void getSurveyQuestionByCourseID() throws SQLException, AwsSecretsManagerException {
-		List<Question> questionList=surveyService.getSurveyQuestionByCourseID(1);
-		int i=0;
-		for(i=0;i<questionList.size();i++){
-			Question question = questionList.get(i);
-			assertEquals(question.getQuestionId(), i);
-		}
+        List<Question> questionList = surveyService.getSurveyQuestionByCourseID(1);
+        int i = 0;
+        for (i = 0; i < questionList.size(); i++) {
+            Question question = questionList.get(i);
+            assertEquals(question.getQuestionId(), i);
+        }
     }
 
     @Test
@@ -51,11 +51,18 @@ class SurveyServiceTest {
     }
 
     @Test
-    void createSurveyIfNotExists() {
+    void createSurveyIfNotExists() throws SQLException, AwsSecretsManagerException {
+        Survey survey = null;
+        assertNull(survey);
+
+        survey = surveyService.getSurveyByCourseId(1);
+        assertNotNull(survey);
     }
 
     @Test
-    void addQuestionToSurvey() {
+    void addQuestionToSurvey() throws SQLException, AwsSecretsManagerException {
+        Survey survey = surveyService.getSurveyByCourseId(1);
+        assertNotNull(survey);
     }
 
     @Test
